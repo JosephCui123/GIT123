93eb052a538f3f504a5ebbab9a7257c86166fb0f
CUI <1394983791@qq.com>
2022-09-03 12:06:06 +0800

    111 222

---
+++ src/main/java/wugong/dugujiujian.java
@@ -0,0 +1,12 @@
+package wugong;
+
+public class dugujiujian {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.err.println("1111111111");
+		System.err.println("22222");
+	}
+
+}
